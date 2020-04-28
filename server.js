ae2d6f164a87fad948df6510bd88e8195e3e65d1
Laura MacKew <48992747+l-mack@users.noreply.github.com>
2020-04-28 12:41:27 -0400

    added full stack

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
+// register the static "middleware"
+const folderInWhichToLook = "public";
+const staticMiddleware = express.static(folderInWhichToLook);
+app.use(staticMiddleware);
 
-app.get('/', (req, res) => {
-  // used to be 
-  // res.send('Hello World')
+let numberOfClicks = 0;
+
+// i changed my route because i want my `index.html` to be server from the root
+app.get("/api/json", (req, res) => {
   const json = {};
-  json.message = 'Hello World';
-  json.otherMessage = 'Foo Bar Baz';
+  json.message = "Hello!! World!!!!!";
+  json.otherMessage = "Foo Bar Baz";
+  json.numberOfClicks = numberOfClicks;
 
-  res.json(json); // returns json object as created above
+  res.json(json);
+});
+
+app.post("/api/json", (req, res) => {
+  numberOfClicks++;
+  res.redirect("/");
 });
 
 app.listen(3000, () => console.log(`Now listening on localhost:3000`));
